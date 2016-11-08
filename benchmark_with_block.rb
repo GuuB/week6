ceb31144daf3358658287db6ab1c2dc22663b6e4
guub <yugbooth@hotmail.com>
2016-11-08 18:13:05 +0000

    adding benchmark function

---
+++ benchmark_with_block.rb
@@ -0,0 +1,15 @@
+def benchmark
+  start = Time.now
+  yield
+  finish = Time.now
+
+  diff = finish - start
+end
+
+# Be careful, pasting this into IRB will take a long time to print.
+# It's a loooong string. :)
+long_string = "apple"*100000000
+
+running_time = benchmark { long_string.reverse }
+
+puts "string.reverse took #{running_time} seconds to run"
---
+++ ruby.rb
@@ -0,0 +1,7 @@
+read = ARGV[2];
+
+def hello_world(name)
+  puts name << " says Hello World!"
+end
+
+hello_world(read)
